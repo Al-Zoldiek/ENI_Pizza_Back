51c5a41047c47e59beb7081f84d9439f5ccbbe51
AD\arebiere2018 <alexis.rebiere2018@campus-eni.fr>
2021-12-06 16:59:47 +0100

    First commit

---
+++ src/main/java/fr/eni/ecole/app/EniPizzaBackApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.ecole.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EniPizzaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EniPizzaBackApplication.class, args);
+	}
+
+}
