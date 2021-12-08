6ac0948f8f9213cb362a3907984836ae47bf3f51
AD\arebiere2018 <alexis.rebiere2018@campus-eni.fr>
2021-12-08 16:25:49 +0100

    Refacto OrderController

--- src/main/java/fr/eni/ecole/app/orders/OrderController.java
+++ src/main/java/fr/eni/ecole/app/orders/OrderController.java
@@ -4,15 +4,11 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import fr.eni.ecole.app.pizzas.Pizza;
 
 
 @RestController
