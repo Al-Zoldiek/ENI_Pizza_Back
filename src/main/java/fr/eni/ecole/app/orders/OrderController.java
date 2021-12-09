2254782751229c1de78c324a826e159a0e7ef802
AD\achervet2021 <annegaelle.chervet2021@campus-eni.fr>
2021-12-09 15:28:35 +0100

    commit ajout de @CrossOrigin sur le OrderController

--- src/main/java/fr/eni/ecole/app/orders/OrderController.java
+++ src/main/java/fr/eni/ecole/app/orders/OrderController.java
@@ -5,13 +5,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
-
+@CrossOrigin(origins="*")
 @RestController
 public class OrderController {
 
